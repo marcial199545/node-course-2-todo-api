9a0a4446706c555944b6fcbe2d2f6e22b462e9f9
Marcial jimenez <marcial.jimenez.caballero@gmail.com>
2019-05-02 09:38:00 -0700

    add delete script

--- playground-dist/mongodb-delete.js
+++ playground-dist/mongodb-delete.js
@@ -17,13 +17,16 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
     console.log("connected to MognoDb");
     var db = client.db("TodoApp");
     // NOTE deleteOne() implementation
-    db.collection("Todos")
-        .deleteOne({ text: "eat lunch" })
-        .then(function (result) {
-        console.log(result.result);
-    }, function (err) {
-        console.log("TCL: err", err);
-    });
+    // db.collection("Todos")
+    //     .deleteOne({ text: "eat lunch" })
+    //     .then(
+    //         result => {
+    //             console.log(result.result);
+    //         },
+    //         err => {
+    //             console.log("TCL: err", err);
+    //         }
+    //     );
     // NOTE deleteMany() implementation
     // db.collection("Todos")
     //     .deleteOne({ text: "eat lunch" })
@@ -35,4 +38,33 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
     //             console.log("TCL: err", err);
     //         }
     //     );
+    // NOTE findOneAndDelete() implementation
+    // db.collection("Todos")
+    //     .findOneAndDelete({ completed: false })
+    //     .then(
+    //         result => {
+    //             console.log(result.value);
+    //         },
+    //         err => {
+    //             console.log("TCL: err", err);
+    //         }
+    //     );
+    //NOTE the same but in users
+    // db.collection("Users")
+    //     .deleteMany({ name: "Marcial" })
+    //     .then(
+    //         result => {
+    //             console.log(result.result);
+    //         },
+    //         err => {
+    //             console.log("TCL: err", err);
+    //         }
+    //     );
+    db.collection("Users")
+        .findOneAndDelete({ _id: new ObjectId("5cc9e8f091dade2ee0aee709") })
+        .then(function (result) {
+        console.log("TCL: result", result.value);
+    }, function (err) {
+        console.log("TCL: err", err);
+    });
 });
--- playground/mongodb-delete.ts
+++ playground/mongodb-delete.ts
@@ -1,4 +1,5 @@
 import * as mongo from "mongodb";
+import { log } from "util";
 const { MongoClient, ObjectId } = mongo;
 MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }, (err: mongo.MongoError, client: mongo.MongoClient) => {
     if (err) {
@@ -8,16 +9,16 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
     console.log(`connected to MognoDb`);
     const db = client.db("TodoApp");
     // NOTE deleteOne() implementation
-    db.collection("Todos")
-        .deleteOne({ text: "eat lunch" })
-        .then(
-            result => {
-                console.log(result.result);
-            },
-            err => {
-                console.log("TCL: err", err);
-            }
-        );
+    // db.collection("Todos")
+    //     .deleteOne({ text: "eat lunch" })
+    //     .then(
+    //         result => {
+    //             console.log(result.result);
+    //         },
+    //         err => {
+    //             console.log("TCL: err", err);
+    //         }
+    //     );
 
     // NOTE deleteMany() implementation
     // db.collection("Todos")
@@ -30,4 +31,36 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
     //             console.log("TCL: err", err);
     //         }
     //     );
+    // NOTE findOneAndDelete() implementation
+    // db.collection("Todos")
+    //     .findOneAndDelete({ completed: false })
+    //     .then(
+    //         result => {
+    //             console.log(result.value);
+    //         },
+    //         err => {
+    //             console.log("TCL: err", err);
+    //         }
+    //     );
+    //NOTE the same but in users
+    // db.collection("Users")
+    //     .deleteMany({ name: "Marcial" })
+    //     .then(
+    //         result => {
+    //             console.log(result.result);
+    //         },
+    //         err => {
+    //             console.log("TCL: err", err);
+    //         }
+    //     );
+    db.collection("Users")
+        .findOneAndDelete({ _id: new ObjectId("5cc9e8f091dade2ee0aee709") })
+        .then(
+            result => {
+                console.log("TCL: result", result.value);
+            },
+            err => {
+                console.log("TCL: err", err);
+            }
+        );
 });
